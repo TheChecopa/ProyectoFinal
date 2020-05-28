09e1167d3f75f8d8f108ddc7acdde2791695c252
Cholico <alejandro.cholico@alumnos.udg.mx>
2020-05-28 02:04:51 -0500

    Final

---
+++ Creadora.h
@@ -0,0 +1,26 @@
+#ifndef CREADORA_H
+#define CREADORA_H
+#include <iostream>
+#include "Instruccion.h"
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+
+class Creadora
+{
+    public:
+        Creadora();
+
+    void Ingreso(string valor);
+
+
+    string p;
+
+
+    private:
+
+};
+
+#endif // CREADORA_H
